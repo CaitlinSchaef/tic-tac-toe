79ede43f642724a5225eab78e84621aea645314f
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-18 13:51:26 -0400

    updated javascript

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
 // Let's go!
 
+const { ModuleNode } = require("vite");
+
 const resetButton = document.getElementById('resetButton')
 const playerTurn = document.getElementById('playerTurn')
+let currentPlayer; 
 
-//
+
+//Function to switch between player control of the screen
+
+    function whoseTurn() {
+        currentPlayer = 
+    }
+
+//Function to display player turn
+
+    function displayTurn() {
+
+    }
+
+//Function to make game work (icons appear in box when clicked)
+
+    function gamePlay() {
+
+    }
+
+//Function to display player score
+
+//Function to declare winner
+
+//Function to reset page
+
+
