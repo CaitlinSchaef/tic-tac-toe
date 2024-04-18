d7fc7fcefc1223d65895b07f498c1d1113f8a699
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-18 15:14:20 -0400

    the board works

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // Let's go!
 
-const { ModuleNode } = require("vite");
 
 const winningMessage = () => `Player ${currentPlayer} won the game!`
 const drawMessage = () => `It's a draw!`
@@ -8,32 +7,54 @@ const resetButton = document.getElementById('resetButton')
 const playerTurn = document.getElementById('playerTurn')
 const scoreDisplay1 = document.getElementById('scoreDisplay1')
 const scoreDisplay2 = document.getElementById('scoreDisplay2')
-let currentPlayer; 
+let currentPlayer = 'playerOne' // 'playerOne' || 'playerTwo'
+
+let boxes = document.querySelectorAll('.tic-box')
+boxes.forEach(currentBox => {
+    currentBox.addEventListener('click', () => gamePlay(currentBox))
+})
+
+
+// this works!
+// const topLeft = document.getElementById('topLeft')
+
+// // giving it a blank function so that it does not inadvertently call the gamePlay function
+// topLeft.addEventListener('click', () => gamePlay(topLeft))
+
 
 
 //Function to switch between player control of the screen
 
-    function whoseTurn() {
-        currentPlayer = {
-            if(player1) {
-            != player2
-            } else if (player2) {
-                != player1
-            }
-        } return currentPlayer
-        console.log(currentPlayer)
-    }
+    // function whoseTurn() {
+    //     currentPlayer = {
+    //         if(player1) {
+    //         ! player2
+    //         } else if (player2) {
+    //             ! player1
+    //         }
+    //     } return currentPlayer
+    //     console.log(currentPlayer)
+    // }
+
 
 //Function to display player turn
 
-    function displayTurn() {
+    // function displayTurn() {
 
-    }
+    // }
 
 //Function to make game work (icons appear in box when clicked)/board state
 
-    function gamePlay() {
-
+    function gamePlay(currentBox) {
+        console.log('WORK')
+        console.log(currentBox)
+       if (currentPlayer === 'playerOne') {
+        currentBox.textContent = 'X'
+        currentPlayer = 'playerTwo'
+       } else if (currentPlayer === 'playerTwo') {
+            currentBox.textContent = 'O'
+            currentPlayer = 'playerOne'
+       } 
     }
 
 //Function to display player score
