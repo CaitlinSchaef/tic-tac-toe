b3d019eba7ed453d66c641aef86808ed17f68636
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-18 14:12:46 -0400

    wokring on java still

--- js/main.js
+++ js/main.js
@@ -2,15 +2,26 @@
 
 const { ModuleNode } = require("vite");
 
+const winningMessage = () => `Player ${currentPlayer} won the game!`
+const drawMessage = () => `It's a draw!`
 const resetButton = document.getElementById('resetButton')
 const playerTurn = document.getElementById('playerTurn')
+const scoreDisplay1 = document.getElementById('scoreDisplay1')
+const scoreDisplay2 = document.getElementById('scoreDisplay2')
 let currentPlayer; 
 
 
 //Function to switch between player control of the screen
 
     function whoseTurn() {
-        currentPlayer = 
+        currentPlayer = {
+            if(player1) {
+            != player2
+            } else if (player2) {
+                != player1
+            }
+        } return currentPlayer
+        console.log(currentPlayer)
     }
 
 //Function to display player turn
@@ -19,7 +30,7 @@ let currentPlayer;
 
     }
 
-//Function to make game work (icons appear in box when clicked)
+//Function to make game work (icons appear in box when clicked)/board state
 
     function gamePlay() {
 
