6de6a24057db34086c079a4a3cd371b61e5881e7
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-19 08:09:46 -0400

    fixed order of messages

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ const resetButton = document.getElementById('resetButton')
 const playerTurn = document.getElementById('playerTurn')
 const scoreDisplay1 = document.getElementById('scoreDisplay1')
 const scoreDisplay2 = document.getElementById('scoreDisplay2')
+const displayWinner = document.getElementById('displayWinner')
 
 
 let currentPlayer = 'playerOne' // 'playerOne' || 'playerTwo'
@@ -46,12 +47,12 @@ boxes.forEach(currentBox => {
 
        if (currentPlayer === 'playerOne') {
             currentBox.textContent = 'X'
-            playerTurn.textContent = "Player 1's Turn"
+            playerTurn.textContent = "Player 2's Turn"
             checkWin()
             currentPlayer = 'playerTwo'
        } else if (currentPlayer === 'playerTwo') {
             currentBox.textContent = 'O'
-            playerTurn.textContent = "Player 2's Turn"
+            playerTurn.textContent = "Player 1's Turn"
             checkWin()
             currentPlayer = 'playerOne'
        } // every time you click you go through this once
@@ -66,8 +67,6 @@ function checkWin() {
     
     // (if the function returns true for any of the array items returns true)
    let winner = winningRows.some(currentArray =>  {
-        console.log()
-        console.log()
         return currentArray.every(x => x.textContent === 'X') || currentArray.every(x => x.textContent === 'O')
         // if (currentArray[0].textContent === '' || currentArray[1].textContent === '' 
         // || currentArray[2].textContent === '') {
@@ -78,7 +77,7 @@ function checkWin() {
     });  
     console.log(winner)
     if (winner === true) {
-        window.alert(`${currentPlayer} won!`)
+        displayWinner.textContent = `${currentPlayer} won!`
         }
         // maybe take out the alert and just make a text box???
         
@@ -91,7 +90,7 @@ function checkWin() {
 
 function checkDraw() {
     let draw = boxes.every(content => boxes.textcontent) //you're in the middle of writing this
-    window.alert(`It's a draw!`)
+    displayWinner.textContent = "It's a draw!"
 }
 
 //Function to display player score
